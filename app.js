ce0a161b550ee97f1747e8ea20152702d7c70cd1
johnplatais <P3@nut2019>
2019-04-29 13:55:51 -0500

    Made all changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", function(reaq, res){
     res.sendFile(__dirname + '/index.html')
 })
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 var server = app.listen(port, function(req, res){
     console.log("Catch the action at http://localhost:" + port)
